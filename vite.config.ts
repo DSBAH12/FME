72d25919bf7e1bf024449806fdfaa810ba6b6b2c
BAH12 <abubakarradonisbah@gmail.com>
2026-02-17 02:45:47 +0000

    I made some changes on "vite config" and "package.json"

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/FME/",
   plugins: [react()],
   server: {
     port: 5173,
   },
+  base: "/FME",
 });
