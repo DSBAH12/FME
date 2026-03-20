851863072ed885af50b330e31ae37b0f66e9eeed
BAH12 <abubakarradonisbah@gmail.com>
2026-03-20 03:37:15 +0100

    uncomment out the base:

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  // base: "/FME/",
+  base: "/FME/",
   plugins: [react()],
   server: {
     port: 5173,
